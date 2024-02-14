60f19ce5e6bd276d84d10ad6f0fc6a1261d9a13a
riyufuchi <krevelixtv@gmail.com>
2024-02-14 15:23:27 +0100

    Update 2.1
    Improved:
    * Method that creates application manual
    * Code readability

--- src/code/consoleUtils/ConsoleUtility.cpp
+++ src/code/consoleUtils/ConsoleUtility.cpp
@@ -2,12 +2,12 @@
 // Name        : ConsoleUtility
 // Author      : Riyufuchi
 // Created on  : 27.10.2021
-// Last Edit   : 19.12.2023
+// Last Edit   : 14.12.2024
 //============================================================================
 
 #include "../../inc/ConsoleUtility.h"
-using namespace ConsoleUtils;
-
+namespace ConsoleUtils
+{
 ConsoleUtility::ConsoleUtility()
 {
 }
@@ -107,47 +107,40 @@ void ConsoleUtility::listFilesInFolder(std::string workspacePath)
 void ConsoleUtility::createManual(std::string* args, int lenght)
 {
 	int lineLenght = 1;
-	int lineTmp = 0;
+	int lineLengthTemp = 0;
 	int columnWidth = 0;
-	int xIndex = 0;
-	for(int i = 0; i < lenght; i++)
+	int x = 0;
+	size_t pipeIndex = 0;
+	for (int i = 0; i < lenght; i++)
 	{
-		lineTmp = args[i].length();
-		while(args[i][xIndex] != '|')
-			xIndex++;
-		if(lineTmp > lineLenght)
-			lineLenght = lineTmp;
-		if(xIndex > columnWidth)
-			columnWidth = xIndex;
-		xIndex = 0;
+		lineLengthTemp = args[i].length();
+		pipeIndex = args[i].find('|');
+		if (pipeIndex == std::string::npos)
+			pipeIndex = lineLengthTemp;
+		if (lineLengthTemp > lineLenght)
+			lineLenght = lineLengthTemp;
+		if (pipeIndex > static_cast<size_t>(columnWidth))
+			columnWidth = pipeIndex;
 	}
-	char line[(lineTmp = lineLenght + columnWidth + 20)];
-	for(xIndex = 0; xIndex < lineTmp; xIndex++)
-		line[xIndex] = '-';
+	std::string line(lineLenght + columnWidth, '-');
 	std::cout << line << "\n";
-	for(int y = 0; y < lenght; y++)
+	for (int y = 0; y < lenght; y++)
 	{
-		xIndex = 0;
-		while(args[y][xIndex] != '|')
-		{
-			std::cout << args[y][xIndex];
-			xIndex++;
-		}
-		for(lineTmp = xIndex; lineTmp < columnWidth; lineTmp++)
-		{
-			std::cout << " ";
-		}
-		if(args[y][xIndex] == '|')
-			xIndex++;
+		x = 0;
+		while (args[y][x] != '|')
+			std::cout << args[y][x++];
+		std::cout << std::string(columnWidth - x, ' ');
+		if (args[y][x] == '|')
+			x++;
 		std::cout << " |";
 		lineLenght = args[y].length();
-		for(; xIndex < lineLenght; xIndex++)
-			std::cout << args[y][xIndex];
-		std::cout << " \n";
+		for (; x < lineLenght; x++)
+			std::cout << args[y][x];
+		std::cout << "\n";
 	}
 	std::cout << line << "\n";
 }
-
 ConsoleUtility::~ConsoleUtility()
 {
 }
+} // Namespace
